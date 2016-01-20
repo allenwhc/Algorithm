9e65d57d914f777449311bf0d61f89deb67ba132
Alan <hcallen.wang@gmail.com>
2016-01-20 16:06:14 -0500

    1/20/16

--- Hash/MaximumSubarraySUm/MaxSum.java
+++ Hash/MaximumSubarraySUm/MaxSum.java
@@ -6,20 +6,22 @@ public class MaxSum{
 		int n=nums.length;
 		if(n==0) return 0;
 		int len=0;
+		for(int i=1; i<n; i++) nums[i]=nums[i]+nums[i-1];
 		HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
+		map.put(0,-1);
 		int sum=0;
 		for(int i=0; i<n; i++){
-			sum+=nums[i];
-			if(sum==k) len=i+1;
-			else if(map.containsKey(sum-k)) len=Math.max(len,map.get(sum-k));
-			if(!map.containsKey(sum)) map.put(sum,i); 
+			if(map.containsKey(nums[i]-k))
+				len=Math.max(len,i-map.get(nums[i]-k));
+			if(!map.containsKey(nums[i]))
+				map.put(nums[i],i);
 		}
 		return len;
 	}
 
 	public static void main(String[] args) {
-		int[] nums={1,-1,5,-2,3};
-		int k=3;
+		int[] nums={-1,1};
+		int k=1;
 		System.out.println("Max length = "+maxSubArrayLen(nums,k));
 	}
 }
---
+++ Heap/RemoveDuplicateLetter/RemoveDuplicateLetters.java
@@ -0,0 +1,12 @@
+
+public class RemoveDuplicateLetters{
+	public static String removeDuplicateLetters(String s){
+		if(s.length()==0 || s==null) return "";
+			
+	}
+
+	public static void main(String[] args) {
+		String s="cbacdcbc";
+		System.out.println("'"+s+"' -> '"+removeDuplicateLetters(s)+"'.");
+	}
+}
